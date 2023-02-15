ff5c86e9656157a29fe04f518b305715a394ed42
hgrafa <hugorafell@gmail.com>
2023-02-15 12:19:58 -0300

    fix: Generation id type to identitity

--- src/main/java/br/com/resilia/smartclasses/domain/model/Course.java
+++ src/main/java/br/com/resilia/smartclasses/domain/model/Course.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor @AllArgsConstructor
 public class Course {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
--- src/main/java/br/com/resilia/smartclasses/domain/model/Lesson.java
+++ src/main/java/br/com/resilia/smartclasses/domain/model/Lesson.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class Lesson {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private LocalDateTime moment;
