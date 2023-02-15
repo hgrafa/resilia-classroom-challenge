6b51b5c5d46c59735d3a6533a17e599301973ab9
hgrafa <hugorafell@gmail.com>
2023-02-15 09:52:40 -0300

    fix: jpa annotation and jpa incorrect interface extension

--- src/main/java/br/com/resilia/smartclasses/repository/CourseRepository.java
+++ src/main/java/br/com/resilia/smartclasses/repository/CourseRepository.java
@@ -1,10 +1,14 @@
 package br.com.resilia.smartclasses.repository;
 
+import br.com.resilia.smartclasses.domain.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface Course extends JpaRepository<Course, Long> {
+import java.util.Optional;
 
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Optional<Course> findByName(String courseName);
 
 }
