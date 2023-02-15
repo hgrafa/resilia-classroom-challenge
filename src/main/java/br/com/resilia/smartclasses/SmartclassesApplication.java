06f1236c9d8b741aae2a378e5eddfc5d1ff9fc6e
hgrafa <hugorafell@gmail.com>
2023-02-15 08:57:54 -0300

    chore: adding main application

---
+++ src/main/java/br/com/resilia/smartclasses/SmartclassesApplication.java
@@ -0,0 +1,13 @@
+package br.com.resilia.smartclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartclassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartclassesApplication.class, args);
+	}
+
+}
