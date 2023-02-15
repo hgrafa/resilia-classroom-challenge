394e15eab3d1b21951fc0bf2efd4eb51b7977e8e
hgrafa <hugorafell@gmail.com>
2023-02-15 12:20:24 -0300

    feat: adding post and get endpoints to Team

--- src/main/java/br/com/resilia/smartclasses/resources/TeamController.java
+++ src/main/java/br/com/resilia/smartclasses/resources/TeamController.java
@@ -1,18 +1,37 @@
 package br.com.resilia.smartclasses.resources;
 
+import br.com.resilia.smartclasses.domain.dto.NewTeamRequest;
 import br.com.resilia.smartclasses.domain.dto.NewTeamResponse;
+import br.com.resilia.smartclasses.domain.dto.TeamResponse;
+import br.com.resilia.smartclasses.repository.TeamRepository;
 import br.com.resilia.smartclasses.services.TeamService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/teams")
-@AllArgsConstructor
 public class TeamController {
 
+    @Autowired
     private TeamService teamService;
 
-    public ResponseEntity<NewTeamResponse>
+    @GetMapping
+    public ResponseEntity<List<TeamResponse>> getAllTeams(
+            @RequestParam(name="active", defaultValue = "false") boolean active
+    ) {
+        var teams = active ? teamService.getAllActive() : teamService.getAll();
+        var teamsReponse = teams.stream().map(TeamResponse::new).toList();
+        return ResponseEntity.ok(teamsReponse);
+    }
+
+    @PostMapping ResponseEntity<NewTeamResponse> addNewTeam(@RequestBody NewTeamRequest newTeamRequest) {
+        var teamRegisted = teamService.registerTeam(newTeamRequest);
+        var teamResponse = new NewTeamResponse(teamRegisted);
+        return ResponseEntity.ok(teamResponse);
+    }
+
 }
