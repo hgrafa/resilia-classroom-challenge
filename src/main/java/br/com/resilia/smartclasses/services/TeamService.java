0a9fa831650d6afc88a305a78f35eaac5b52215f
hgrafa <hugorafell@gmail.com>
2023-02-15 12:23:43 -0300

    feat: adding services to save teams and new courses

--- src/main/java/br/com/resilia/smartclasses/services/TeamService.java
+++ src/main/java/br/com/resilia/smartclasses/services/TeamService.java
@@ -1,11 +1,18 @@
 package br.com.resilia.smartclasses.services;
 
 import br.com.resilia.smartclasses.domain.dto.NewTeamRequest;
+import br.com.resilia.smartclasses.domain.dto.NewTeamResponse;
 import br.com.resilia.smartclasses.domain.model.Course;
+import br.com.resilia.smartclasses.domain.model.Team;
 import br.com.resilia.smartclasses.repository.CourseRepository;
 import br.com.resilia.smartclasses.repository.TeamRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,8 +20,25 @@ public class TeamService {
 
     private TeamRepository teamRepository;
     private CourseRepository courseRepository;
-    public boolean registerTeam(NewTeamRequest newTeamRequest) {
-        return true;
+
+    public List<Team> getAll() {
+        return teamRepository.findAll();
+    }
+
+    public List<Team> getAllActive() {
+        return teamRepository.findAll().stream()
+                .filter(Team::isActive).toList();
+    }
+
+    public Team getTeamByName(String name) {
+        // TODO exception handle when there is no team with this name
+        return teamRepository.findByName(name).get();
+    }
+
+    public Team registerTeam(NewTeamRequest newTeamRequest) {
+        // TODO register team
+        var teamToRegister = toTeam(newTeamRequest);
+        return teamRepository.save(teamToRegister);
     }
 
     private Course getCourse(String courseName) {
@@ -26,6 +50,18 @@ public class TeamService {
         }
 
         return course.orElse(newCourse);
+    }
+
+    private Team toTeam(NewTeamRequest newTeamRequest) {
+        var courseOfTeam = getCourse(newTeamRequest.getCourseName());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return Team.builder()
+                .course(courseOfTeam)
+                .name(newTeamRequest.getName())
+                .startDate(LocalDateTime.parse(newTeamRequest.getStart(), formatter))
+                .endDate(LocalDateTime.parse(newTeamRequest.getEnd(), formatter))
+                .students(new ArrayList<>())
+                .build();
     }
 
     private Course toCourse(String courseName) {
