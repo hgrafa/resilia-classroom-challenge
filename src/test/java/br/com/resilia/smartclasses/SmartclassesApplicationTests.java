3289a7a02e97a08e6894831df701d6ce0dd19d4e
hgrafa <hugorafell@gmail.com>
2023-02-15 08:56:49 -0300

    test: adding test folder

---
+++ src/test/java/br/com/resilia/smartclasses/SmartclassesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.resilia.smartclasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartclassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
