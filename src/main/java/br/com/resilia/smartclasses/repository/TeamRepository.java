3f969db7f4324713f01c2feb90721830122015aa
hgrafa <hugorafell@gmail.com>
2023-02-15 12:22:52 -0300

    chore: adding methods to team repository

--- src/main/java/br/com/resilia/smartclasses/repository/TeamRepository.java
+++ src/main/java/br/com/resilia/smartclasses/repository/TeamRepository.java
@@ -4,8 +4,11 @@ import br.com.resilia.smartclasses.domain.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
+    Optional<Team> findByName(String name);
 
 }
